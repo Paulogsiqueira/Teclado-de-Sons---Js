bab0ccb7fbf77bd1534a931589e1d803c6d5bfbf
Paulo Gustavo Siqueira <99617968+Paulogsiqueira@users.noreply.github.com>
2022-08-02 15:58:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function tocarSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    
+    if( elemento && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        alert('Element não encontrado ou seletor inválido')
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for( var i=0;i < listaDeTeclas.length; i++){
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocarSom(idAudio);
+    };
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space"){
+            tecla.classList.add('ativa');
+    }}
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+}
